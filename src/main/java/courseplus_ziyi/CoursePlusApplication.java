68d18a3a36f6c41648212ec62ade8f83b63ee74e
ziyifeng <ziyifeng527@gmail.com>
2024-04-01 13:09:16 -0400

    init repo

---
+++ src/main/java/courseplus_ziyi/CoursePlusApplication.java
@@ -0,0 +1,13 @@
+package courseplus_ziyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursePlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursePlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/courseplus_ziyi/CoursePlusApplicationTests.java
@@ -0,0 +1,13 @@
+package courseplus_ziyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursePlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
